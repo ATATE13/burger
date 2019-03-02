dfe14f7e61be17bc5648a3e86e57c3d21cc17728
atate13 <atate13@yahoo.com>
2019-03-02 15:52:15 -0500

    updating code

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+    -- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Staked Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sharp Cheddar Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salsa Lime Burger', true);
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "aKt121379",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
+
